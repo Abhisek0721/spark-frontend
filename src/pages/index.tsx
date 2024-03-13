5a99a098377c2a9df864f0d2643a499243d54f8c
Abhisekh Upadhaya <Abhisek0721@gmail.com>
2024-03-13 22:44:22 +0530

    feat - basic directory structure

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-red-800 underline text-3xl text-center">
+                Hello World!
+            </h1>
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/pages/login.tsx
@@ -0,0 +1,11 @@
+const Login = ():JSX.Element => {
+    return (
+        <div>
+            <h1 className="text-center text-blue-700 font-bold">
+                This is login page!
+            </h1>
+        </div>
+    )
+}
+
+export default Login;
